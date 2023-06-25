9bc73980f239baf78e0807f39eee8856e1dcdb4e
Andrei <vertru112211@gmail.com>
2023-06-25 12:31:11 +0300

    add voices

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ engine=pyttsx3.init()
 #engine.runAndWait()
 #talk("import os, pyttsx3 import speech_recognition as sr engine=pyttsx3.init() #engine.say('Текст') #engine.runAndWait() def talk(words):    print(words)    #os.system(f'say {words}')    engine.say(words)    engine.runAndWait()")
 
-command('44')
+command(None)
